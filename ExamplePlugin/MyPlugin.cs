992939e7d49c8b63a48eb81a1f3f6e958eafa23e
Deliay <admin@remiliascarlet.com>
2019-11-20 01:44:55 +0800

    Example plugin update command handler part

--- ExamplePlugin/MyPlugin.cs
+++ ExamplePlugin/MyPlugin.cs
@@ -1,29 +1,61 @@
 ﻿using Autofac;
+using EmberCore.KernelServices.Command;
+using EmberCore.KernelServices.Command.Attributes;
+using EmberCore.KernelServices.Command.Components;
+using EmberCore.KernelServices.Command.Models;
+using EmberCore.KernelServices.Command.Parsers;
 using EmberKernel.Plugins;
 using EmberKernel.Plugins.Attributes;
 using EmberKernel.Plugins.Components;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ExamplePlugin
 {
     [EmberPlugin(Author = "ZeroAsh", Name = "ExamplePlugin", Version = "1.0")]
-    public class MyPlugin : Plugin
+    public class MyPlugin : Plugin, ICommandContainer
     {
-        public override void BuildComponents(IComponentBuilder builder)
+        private ICommandServices CommandService { get; }
+        private ILogger<MyPlugin> Logger { get; }
+        public MyPlugin(ICommandServices commandService, ILogger<MyPlugin> logger)
         {
-            
+            CommandService = commandService;
+            Logger = logger;
         }
 
-        public override async Task Initialize(ILifetimeScope scope)
+        private class CustomParser : IParser
         {
-            await Task.Yield();
+            public IEnumerable<object> ParseCommandArgument(CommandArgument args)
+            {
+                if (int.TryParse(args.Argument, out var parsedInt)) yield return parsedInt;
+                else yield return 0;
+            }
+        }
+
+        [CommandHandler(Command = "my", Parser = typeof(CustomParser))]
+        public void MyCommand(int args)
+        {
+            Logger.LogInformation($"My command invoked, args + 1 = {args + 1}");
+        }
+
+        public override Task Initialize(ILifetimeScope scope)
+        {
+            CommandService.ReigsterCommandContainer(this);
+            return Task.CompletedTask;
         }
 
         public override async Task Uninitialize()
         {
             await Task.Yield();
         }
+
+        public override void BuildComponents(IComponentBuilder builder)
+        {
+
+        }
+
     }
 }
