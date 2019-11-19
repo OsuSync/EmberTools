0ab9d7101ee7b3f303a1875b638cace36726babe
Zero.Ash <admin@remiliascarlet.com>
2019-11-19 19:09:59 +0800

    Create CommandService class

---
+++ EmberCore/KernelServices/Command/CommandService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using EmberCore.KernelServices.Command.Components;
+using EmberCore.KernelServices.Command.Parsers;
+
+namespace EmberCore.KernelServices.Command
+{
+    class CommandService : ICommandServices
+    {
+        private Dictionary<ICommandComponent, LinkedList<MethodInfo>> commandHandlers = new Dictionary<ICommandComponent, LinkedList<MethodInfo>>();
+
+        public void Reigster(ICommandComponent commandHandler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unregister(ICommandComponent commandHandler)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
