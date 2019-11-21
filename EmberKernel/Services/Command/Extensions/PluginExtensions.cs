982f20c579e1b1e6681532235d52f9b501fe1e34
Deliay <admin@remiliascarlet.com>
2019-11-22 02:37:55 +0800

    Add a command service extension for register container

---
+++ EmberKernel/Services/Command/Extensions/PluginExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using EmberKernel.Services.Command;
+using EmberKernel.Services.Command.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmberKernel
+{
+    public static class PluginExtensions
+    {
+        public static void UseCommandContainer<T>(this ILifetimeScope scope) where T : ICommandContainer
+        {
+            ICommandService commandService = scope.Resolve<ICommandService>();
+            commandService.ReigsterCommandContainer(scope.Resolve<T>());
+        }
+    }
+}
