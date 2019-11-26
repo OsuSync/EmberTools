d87331030dfd2511745c5fbf77588a18fe13be51
Deliay <admin@remiliascarlet.com>
2019-11-26 20:11:21 +0800

    Create EmberAuthorTools project

---
+++ EmberAuthorTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmberAuthorTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ember author tools!");
+        }
+    }
+}
