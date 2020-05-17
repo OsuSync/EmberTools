f72c8f374dcd5b71e2d1d3f4dcb921892ecc5aa0
Deliay <admin@remiliascarlet.com>
2020-05-18 01:45:15 +0800

    Support comparable event to avoid repeat event publish

--- EmberMemoryReader/Components/Collector/Collectors/CollectorIntervalContainer.cs
+++ EmberMemoryReader/Components/Collector/Collectors/CollectorIntervalContainer.cs
@@ -1,7 +1,9 @@
-﻿using EmberKernel.Services.EventBus;
+﻿using Autofac;
+using EmberKernel.Services.EventBus;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,33 +15,47 @@ namespace EmberMemoryReader.Components.Collector.Collectors
         private ICollector Collector { get; }
         private IEventBus EventBus { get; }
         private ILogger<ICollectorContainer<T>> Logger { get; }
+        private bool EventCanCompare { get; }
         private int _retryCount;
+        private Func<object, object, bool> EventComparer;
+        private object _latestEvent;
         public CollectorIntervalContainer(ILogger<ICollectorContainer<T>> logger, T collector, IEventBus eventBus)
         {
             this.Collector = collector;
             this.EventBus = eventBus;
             this.Logger = logger;
+            var comparableType = typeof(IComparableCollector<>);
+            var correctType = typeof(T).GetInterface(comparableType.Name);
+            if (correctType != null)
+            {
+                EventCanCompare = true;
+                var eventType = correctType.GenericTypeArguments[0];
+                EventComparer = (object last, object now) => (bool)eventType.GetMethod("Equals", new[] { eventType }).Invoke(last, new[] { now });
+            }
         }
 
         public async Task Run(CancellationToken cancellationToken)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                if (Collector.TryInitialize())
-                {
-                    if (!Collector.TryRead(out Event @event)
-                        && ++_retryCount > Collector.RetryLimit)
-                    {
-                        Logger.LogInformation($"Memory reader {Collector.GetType().Name} exceeded max retry limit");
-                        break;
-                    }
-                    EventBus.Publish(@event);
-                }
-                else
+                await Task.Delay(Collector.ReadInterval);
+                if (!Collector.TryInitialize())
                 {
                     Logger.LogDebug($"Memory reader {Collector.GetType().Name} not initialized");
+                    continue;
                 }
-                await Task.Delay(Collector.ReadInterval); 
+
+                if (!Collector.TryRead(out Event @event)
+                    && ++_retryCount > Collector.RetryLimit)
+                {
+                    Logger.LogInformation($"Memory reader {Collector.GetType().Name} exceeded max retry limit");
+                    break;
+                }
+                if (!EventCanCompare || _latestEvent == null || !EventComparer(@event, _latestEvent))
+                {
+                    _latestEvent = @event;
+                    EventBus.Publish(@event);
+                }
             }
         }
     }
