a1d5f530d5e8ee1b40765e0039de2fb0258d7f16
Deliay <admin@remiliascarlet.com>
2020-05-23 20:44:12 +0800

    Add a method to expose async event Task

--- EmberKernel/Services/EventBus/MemoryEventBus.cs
+++ EmberKernel/Services/EventBus/MemoryEventBus.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Autofac;
 using EmberKernel.Services.EventBus.Handlers;
@@ -9,10 +10,10 @@ using Microsoft.Extensions.Logging;
 
 namespace EmberKernel.Services.EventBus
 {
-    public class MemoryEventBus : KernelService, IEventBus
+    public class MemoryEventBus : KernelService, IEventBus, IDisposable
     {
         private readonly ISubscriptionManager _subsManager;
-
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         public MemoryEventBus()
         {
             _subsManager = new EventSubscriptionManager();
@@ -20,12 +21,17 @@ namespace EmberKernel.Services.EventBus
 
         public void Publish(Event @event)
         {
-            var eventName = @event.GetType().GetFullEventName();
-            var jsonMessage = JsonSerializer.Serialize(@event, @event.GetType());
-            Task.Run(() => ProcessEvent(eventName, jsonMessage));
+            Task.Run(() => Publish(@event, _cancellationTokenSource.Token));
         }
 
-        private async Task ProcessEvent(string eventName, string message)
+        public Task Publish(Event @event, CancellationToken cancellation = default)
+        {
+            var eventName = @event.GetType().GetFullEventName();
+            var jsonMessage = JsonSerializer.Serialize(@event, @event.GetType());
+            return ProcessEvent(eventName, jsonMessage, cancellation);
+        }
+
+        private async Task ProcessEvent(string eventName, string message, CancellationToken cancellation = default)
         {
             if (!_subsManager.HasSubscriptionForEvent(eventName)) return;
 
@@ -39,7 +45,15 @@ namespace EmberKernel.Services.EventBus
                 var eventType = _subsManager.GetEventTypeByName(eventName);
                 var @event = JsonSerializer.Deserialize(message, eventType);
                 var concreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
-                await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
+                var method = concreteType.GetMethod("Handle");
+                if (method.GetParameters().Length == 2)
+                {
+                    await (Task)method.Invoke(handler, new object[] { @event, cancellation });
+                }
+                else
+                {
+                    await (Task)method.Invoke(handler, new object[] { @event });
+                }
             }
         }
 
@@ -55,6 +69,12 @@ namespace EmberKernel.Services.EventBus
             where THandler : IEventHandler<TEvent>
         {
             _subsManager.RemoveSubscription<TEvent, THandler>();
+        }
+
+        public void Dispose()
+        {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
         }
     }
 }
