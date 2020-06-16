6ae06036c2ca53aa47887397fdf14b43e1e736e3
Deliay <admin@remiliascarlet.com>
2020-06-16 22:54:31 +0800

    Add WpfCore project

---
+++ EmberWpfCore/UICorePlugin.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using EmberCore.KernelServices.UI.View;
+using EmberKernel.Plugins;
+using EmberKernel.Plugins.Attributes;
+using EmberKernel.Plugins.Components;
+using EmberKernel.Services.UI;
+using EmberWpfCore.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmberWpfCore
+{
+    [EmberPlugin(Author = "Deliay", Name = "CoreUI", Version = "0.0.1")]
+    public class UICorePlugin : Plugin
+    {
+        public UICorePlugin()
+        {
+
+        }
+
+        public override void BuildComponents(IComponentBuilder builder)
+        {
+            builder.ConfigureComponent<WpfUIComponent<Main>>();
+        }
+
+        public override Task Initialize(ILifetimeScope scope)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task Uninitialize(ILifetimeScope scope)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ EmberWpfCore/View/Main.xaml.cs
@@ -0,0 +1,28 @@
+﻿using EmberKernel.Plugins.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace EmberWpfCore.View
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
