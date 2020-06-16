8e4bdc570fcac41c17c04ab74581d36225b6b35f
Deliay <admin@remiliascarlet.com>
2020-06-16 22:51:51 +0800

    EmberCore Support WpfUI

---
+++ EmberCore/KernelServices/UI/View/WpfUIComponent.cs
@@ -0,0 +1,27 @@
+﻿using EmberKernel.Plugins.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace EmberCore.KernelServices.UI.View
+{
+    public class WpfUIComponent<TWindow> : UIComponent
+        where TWindow : Window, new()
+    {
+        private TWindow window;
+        private Application application;
+
+        public override bool Run()
+        {
+            application = new Application();
+            window = new TWindow();
+            application.Run(window);
+            return true;
+        }
+        public override void Stop()
+        {
+            application.Shutdown();
+        }
+    }
+}
