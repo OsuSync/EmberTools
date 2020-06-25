6df10517c1dc2e60122367efe433864019e3c367
Deliay <admin@remiliascarlet.com>
2020-06-26 01:07:04 +0800

    Add OsuDatabase Reader

---
+++ share/OsuSqliteDatabase/Utils/Reader/BinaryReaderExtension.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace OsuSqliteDatabase.Utils.Reader
+{
+    public static class BinaryReaderExtension
+    {
+        public static string ReadOsuString(this BinaryReader reader)
+        {
+            if (0 == reader.ReadByte()) return null;
+            return reader.ReadString();
+        }
+        public static DateTime ReadDateTime(this BinaryReader reader)
+        {
+            return new DateTime(reader.ReadInt64(), DateTimeKind.Utc);
+        }
+        public static byte[] ReadByteArray(this BinaryReader reader)
+        {
+            int len = reader.ReadInt32();
+            if (len > 0) return reader.ReadBytes(len);
+            if (len < 0) return null;
+            return new byte[] { };
+        }
+
+        public static char[] ReadCharArray(this BinaryReader reader)
+        {
+            int len = reader.ReadInt32();
+            if (len > 0) return reader.ReadChars(len);
+            if (len < 0) return null;
+            return new char[] { };
+        }
+        public static Dictionary<T, U> ReadDictionary<T, U>(this BinaryReader reader)
+        {
+            int count = reader.ReadInt32();
+            if (count < 0) return null;
+            Dictionary<T, U> d = new Dictionary<T, U>();
+            for (int i = 0; i < count; i++) d[(T)reader.ReadObject()] = (U)reader.ReadObject();
+            return d;
+        }
+
+        private enum ObjType : byte
+        {
+            Null,
+            Bool,
+            Byte,
+            UShort,
+            UInt,
+            ULong,
+            SByte,
+            Short,
+            Int,
+            Long,
+            Char,
+            String,
+            Float,
+            Double,
+            Decimal,
+            DateTime,
+            ByteArray,
+            CharArray,
+        }
+        public static object ReadObject(this BinaryReader reader)
+        {
+            return (ObjType)reader.ReadByte() switch
+            {
+                ObjType.Bool => reader.ReadBoolean(),
+                ObjType.Byte => reader.ReadByte(),
+                ObjType.UShort => reader.ReadUInt16(),
+                ObjType.UInt => reader.ReadUInt32(),
+                ObjType.ULong => reader.ReadUInt64(),
+                ObjType.SByte => reader.ReadSByte(),
+                ObjType.Short => reader.ReadInt16(),
+                ObjType.Int => reader.ReadInt32(),
+                ObjType.Long => reader.ReadInt64(),
+                ObjType.Char => reader.ReadChar(),
+                ObjType.String => reader.ReadString(),
+                ObjType.Float => reader.ReadSingle(),
+                ObjType.Double => reader.ReadDouble(),
+                ObjType.Decimal => reader.ReadDecimal(),
+                ObjType.DateTime => reader.ReadDateTime(),
+                ObjType.ByteArray => reader.ReadByteArray(),
+                ObjType.CharArray => reader.ReadCharArray(),
+                _ => null,
+            };
+        }
+    }
+}
---
+++ share/OsuSqliteDatabase/Utils/Reader/OsuDatabaseReader.cs
@@ -0,0 +1,142 @@
+﻿using OsuSqliteDatabase.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuSqliteDatabase.Utils.Reader
+{
+    public class OsuDatabaseReader : IAsyncDisposable
+    {
+        private BinaryReader Reader { get; }
+        public OsuDatabaseReader(string path)
+        {
+            Reader = new BinaryReader(File.OpenRead(path), Encoding.UTF8);
+        }
+
+        public OsuDatabase ReadOsuDatabase()
+        {
+            var db = new OsuDatabase();
+            db.Version = Reader.ReadInt32();
+            db.FolderCount = Reader.ReadInt32();
+            db.AccountLocked = Reader.ReadBoolean();
+            db.UnlockedAt = Reader.ReadDateTime();
+            db.PlayerName = Reader.ReadString();
+            db.BeatmapCount = Reader.ReadInt32();
+            db.Beatmaps = ReadBeatmap(db).ToList();
+            db.Permission = (OsuGameBeatmapPermission)Reader.ReadInt32();
+            return db;
+        }
+
+        private IEnumerable<OsuDatabaseBeatmap> ReadBeatmap(OsuDatabase db)
+        {
+            for (int i = 0; i < db.BeatmapCount; i++)
+            {
+                var beatmap = new OsuDatabaseBeatmap();
+                if (db.Version < 20191106) beatmap.BytesOfBeatmapEntry = Reader.ReadInt32();
+                beatmap.Artist = Reader.ReadString();
+                beatmap.ArtistUnicode = Reader.ReadString();
+                beatmap.Title = Reader.ReadString();
+                beatmap.TitleUnicode = Reader.ReadString();
+                beatmap.Creator = Reader.ReadString();
+                beatmap.Difficult = Reader.ReadString();
+                beatmap.AudioFileName = Reader.ReadString();
+                beatmap.MD5Hash = Reader.ReadString();
+                beatmap.FileName = Reader.ReadString();
+                beatmap.RankStatus = (OsuGameBeatmapRankStatus)Reader.ReadByte();
+                beatmap.CircleCount = Reader.ReadInt16();
+                beatmap.SliderCount = Reader.ReadInt16();
+                beatmap.SpinnerCount = Reader.ReadInt16();
+                beatmap.LatestModifiedAt = Reader.ReadDateTime();
+                beatmap.ApproachRate = db.Version >= 20140609 ? Reader.ReadSingle() : Reader.ReadByte();
+                beatmap.CircleSize = db.Version >= 20140609 ? Reader.ReadSingle() : Reader.ReadByte();
+                beatmap.HPDrain = db.Version >= 20140609 ? Reader.ReadSingle() : Reader.ReadByte();
+                beatmap.OverallDifficulty = db.Version >= 20140609 ? Reader.ReadSingle() : Reader.ReadByte();
+                beatmap.SliderVelocity = Reader.ReadDouble();
+                if (db.Version >= 20140609)
+                {
+                    beatmap.StarRatings = ReadStarRatings(beatmap).ToList();
+                }
+                beatmap.DrainTime = Reader.ReadInt32();
+                beatmap.TotalTime = Reader.ReadInt32();
+                beatmap.AudioPreviewTime = Reader.ReadInt32();
+                beatmap.TimingPointCount = Reader.ReadInt32();
+                beatmap.OsuDatabaseTimings = ReadBeatmapTimings(beatmap).ToList();
+                beatmap.BeatmapId = Reader.ReadInt32();
+                beatmap.BeatmapSetId = Reader.ReadInt32();
+                beatmap.ThreadId = Reader.ReadInt32();
+                beatmap.StandardRankRating = (OsuGameRankRating)Reader.ReadByte();
+                beatmap.TaikoRankRating = (OsuGameRankRating)Reader.ReadByte();
+                beatmap.CatchTheBeatRankRating = (OsuGameRankRating)Reader.ReadByte();
+                beatmap.ManiaRankRating = (OsuGameRankRating)Reader.ReadByte();
+                beatmap.LocalOffset = Reader.ReadInt16();
+                beatmap.StackLeniency = Reader.ReadSingle();
+                beatmap.RuleSet = (OsuGameRuleSet)Reader.ReadByte();
+                beatmap.Source = Reader.ReadString();
+                beatmap.Tags = Reader.ReadString();
+                beatmap.OnlineOffset = Reader.ReadInt16();
+                beatmap.TitleFont = Reader.ReadString();
+                beatmap.NotPlayed = Reader.ReadBoolean();
+                beatmap.LatestPlayedAt = Reader.ReadDateTime();
+                beatmap.IsOsz2 = Reader.ReadBoolean();
+                beatmap.FolderName = Reader.ReadString();
+                beatmap.LatestPlayedAt = Reader.ReadDateTime();
+                beatmap.BeatmapSoundIgnored = Reader.ReadBoolean();
+                beatmap.BeatmapSkinIgnored = Reader.ReadBoolean();
+                beatmap.StoryboardDisabled = Reader.ReadBoolean();
+                beatmap.VideoDisabled = Reader.ReadBoolean();
+                beatmap.VisualOverrided = Reader.ReadBoolean();
+                if (db.Version < 20140609)
+                    Reader.BaseStream.Seek(sizeof(short), SeekOrigin.Current);
+                Reader.BaseStream.Seek(sizeof(int), SeekOrigin.Current);
+                beatmap.ManiaScrollSpeed = Reader.ReadByte();
+
+                yield return beatmap;
+            }
+        }
+
+        private IEnumerable<OsuDatabaseBeatmapStarRating> ReadSingleModeRatings(OsuDatabaseBeatmap beatmap, OsuGameRuleSet ruleSet)
+        {
+            foreach (var (moderator, rate) in Reader.ReadDictionary<OsuGameModerator, double>())
+            {
+                yield return new OsuDatabaseBeatmapStarRating()
+                {
+                    Moderators = moderator,
+                    OsuDatabaseBeatmap = beatmap,
+                    RuleSet = ruleSet,
+                    StarRating = rate,
+                };
+            }
+        }
+
+        private IEnumerable<OsuDatabaseBeatmapStarRating> ReadStarRatings(OsuDatabaseBeatmap beatmap)
+        {
+            return ReadSingleModeRatings(beatmap, OsuGameRuleSet.Standard)
+                .Concat(ReadSingleModeRatings(beatmap, OsuGameRuleSet.Taiko))
+                .Concat(ReadSingleModeRatings(beatmap, OsuGameRuleSet.Catch))
+                .Concat(ReadSingleModeRatings(beatmap, OsuGameRuleSet.Mania));
+        }
+
+        private IEnumerable<OsuDatabaseTimings> ReadBeatmapTimings(OsuDatabaseBeatmap beatmap)
+        {
+            for (int i = 0; i < beatmap.TimingPointCount; i++)
+            {
+                yield return new OsuDatabaseTimings()
+                {
+                    BeatPreMinute = Reader.ReadDouble(),
+                    Offset = Reader.ReadDouble(),
+                    Inherited = Reader.ReadBoolean(),
+                    OsuDatabaseBeatmap = beatmap,
+                };
+            }
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            Reader.Dispose();
+            return default;
+        }
+    }
+}
