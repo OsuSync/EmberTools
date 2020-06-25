f5f7fba58a2cf737247aaa6b44469a5a38caa512
Deliay <admin@remiliascarlet.com>
2020-06-25 13:10:02 +0800

    Enhance migrate db context implemention

--- share/EmberSqlite/Extension/PluginDbContextExtension.cs
+++ share/EmberSqlite/Extension/PluginDbContextExtension.cs
@@ -2,6 +2,8 @@
 using EmberKernel.Plugins.Components;
 using EmberSqlite.Integration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Polly;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,14 +24,29 @@ namespace EmberKernel.Plugins
             builder.ConfigureComponent<T>().SingleInstance();
         }
 
-        public static async ValueTask SqliteApplyMigration<T>(this ILifetimeScope scope, CancellationToken cancellationToken = default)
+        public static async ValueTask MigrateDbContext<T>(this ILifetimeScope scope, CancellationToken cancellationToken = default)
             where T : EmberDbContext
         {
+            var logger = scope.Resolve<ILogger<T>>();
             if (!scope.TryResolve<T>(out var sqliteDbContext))
             {
                 throw new NullReferenceException($"Can't resolve {typeof(T).Name}, ensure configured when 'BuildComponent'");
             }
-            await sqliteDbContext.Database.MigrateAsync(cancellationToken);
+            try
+            {
+                logger.LogInformation($"Migrating database with context {typeof(T).Name}");
+                var retry = Policy.Handle<Exception>()
+                    .WaitAndRetryAsync(new TimeSpan[]
+                    {
+                    TimeSpan.FromSeconds(3), TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(8)
+                    });
+                await retry.ExecuteAsync(() => sqliteDbContext.Database.MigrateAsync(cancellationToken));
+                logger.LogInformation($"Migrated database associated with context {typeof(T).Name}");
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"An error occurred while migrating the database used on {typeof(T).Name}");
+            }
         }
     }
 }
