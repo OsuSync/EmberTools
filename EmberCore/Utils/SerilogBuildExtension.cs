7b73884b1d3cd7e4bad5a1e0d6b29dacd0eacc5e
Deliay <admin@remiliascarlet.com>
2020-07-01 01:53:47 +0800

    Add a build extension for build serilog to IoC container

---
+++ EmberCore/Utils/SerilogBuildExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmberCore.Utils
+{
+    public static class SerilogBuildExtension
+    {
+        public static ILoggingBuilder AddSerilog(this ILoggingBuilder builder, Func<LoggerConfiguration, LoggerConfiguration> rootBuilder)
+        {
+            var conf = new LoggerConfiguration();
+            rootBuilder(conf);
+            Log.Logger = conf.CreateLogger();
+
+            builder.AddSerilog(logger: Log.Logger, dispose: true);
+            return builder;
+        }
+    }
+}
