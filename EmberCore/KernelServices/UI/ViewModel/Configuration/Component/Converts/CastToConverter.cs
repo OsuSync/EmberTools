bb8c7fadd1e623ebddf0322d31120e2bffc0368b
Deliay <admin@remiliascarlet.com>
2020-07-04 02:01:40 +0800

    Add builtin configuration component

---
+++ EmberCore/KernelServices/UI/ViewModel/Configuration/Component/Converts/CastToConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace EmberWpfCore.Components.Configuration.View.Component.Converts
+{
+    public abstract class CastToConverter<T> : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => Cast(value);
+
+        public abstract T Cast(object value);
+    }
+}
---
+++ EmberCore/KernelServices/UI/ViewModel/Configuration/Component/IntegerComponent.xaml.cs
@@ -0,0 +1,27 @@
+﻿using EmberWpfCore.Components.Configuration.View.Component.Converts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmberCore.KernelServices.UI.ViewModel.Configuration.Component
+{
+    /// <summary>
+    /// Interaction logic for IntegerComponent.xaml
+    /// </summary>
+    public partial class IntegerComponent : TextBox
+    {
+        public IntegerComponent()
+        {
+            InitializeComponent();
+        }
+    }
+}
