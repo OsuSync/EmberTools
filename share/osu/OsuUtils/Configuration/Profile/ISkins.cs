7d99574cce242ea79bfca005f0bd6415620c4924
Deliay <admin@remiliascarlet.com>
2020-07-07 12:59:34 +0800

    enhance project structure

---
+++ share/osu/OsuUtils/Configuration/Profile/ISkins.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsuUtils.Configuration.Profile
+{
+    public interface ISkins
+    {
+        public string Skin { get; }
+    }
+}
---
+++ share/osu/OsuUtils/Configuration/Profile/Misc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsuUtils.Configuration.Profile
+{
+    public interface IMisc
+    {
+        public string BeatmapDirectory { get; }
+        public string LastVersion { get; }
+        public string ChatChannels { get; }
+    }
+}
