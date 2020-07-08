b718f117982a40e2fbc902dd43b9cd5c0e90d5a9
Deliay <admin@remiliascarlet.com>
2020-07-08 23:31:29 +0800

    IStatisticService and IProperty abstract

---
+++ EmberKernel/Services/Statistic/IStatisticService.cs
@@ -0,0 +1,42 @@
+﻿using EmberKernel.Services.EventBus;
+using EmberKernel.Services.Statistic.Property;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace EmberKernel.Services.Statistic
+{
+    public interface IStatisticService
+    {
+        /// <summary>
+        /// Configure a registered event to statistic services
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        void ConfigureStatistic<TEvent>() where TEvent : Event<TEvent>;
+        /// <summary>
+        /// Get cached category statistic
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <returns></returns>
+        TEvent Current<TEvent>() where TEvent : Event<TEvent>;
+        /// <summary>
+        /// Get cached property statistic
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        TValue Current<TEvent, TValue>(Expression<Func<TEvent, TValue>> property) where TEvent : Event<TEvent>;
+        /// <summary>
+        /// Get all available property
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<IProperty> GetAllProperty();
+        /// <summary>
+        /// Get one property statistic
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        IProperty Current(string property);
+    }
+}
---
+++ EmberKernel/Services/Statistic/Property/IProperty.cs
@@ -0,0 +1,13 @@
+﻿namespace EmberKernel.Services.Statistic.Property
+{
+    public interface IProperty
+    {
+        string Name { get; set; }
+        PropertyDataType DataType { get; }
+        object Data { get; set; }
+    }
+    public interface IProperty<T> : IProperty
+    {
+        new T Data { get; set; }
+    }
+}
