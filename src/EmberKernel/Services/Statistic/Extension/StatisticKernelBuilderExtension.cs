afd887a3b6ea4ae3ad89d59b53b6a840bfe25964
Deliay <admin@remiliascarlet.com>
2020-07-23 22:35:37 +0800

    Add formatter implement class

--- src/EmberKernel/Services/Statistic/Extension/StatisticKernelBuilderExtension.cs
+++ src/EmberKernel/Services/Statistic/Extension/StatisticKernelBuilderExtension.cs
@@ -1,15 +1,17 @@
 ﻿using Autofac;
 using EmberKernel.Services.Statistic;
 using EmberKernel.Services.Statistic.DataSource;
+using EmberKernel.Services.Statistic.Formatter;
 
 namespace EmberKernel
 {
     public static class StatisticKernelBuilderExtension
     {
-        public static KernelBuilder UseStatisticDataSource(this KernelBuilder builder)
+        public static KernelBuilder UseStatistic(this KernelBuilder builder)
         {
             builder.Configure((context, container) =>
             {
+                container.RegisterType<EmberFormatter>().As<IFormatter>().SingleInstance();
                 container.RegisterType<EmberDataSource>().As<IDataSource>().SingleInstance();
             });
             return builder;
---
+++ src/EmberKernel/Services/Statistic/Formatter/EmberFormatter.cs
@@ -0,0 +1,28 @@
+﻿using EmberKernel.Services.Statistic.Format;
+using System;
+
+namespace EmberKernel.Services.Statistic.Formatter
+{
+    public class EmberFormatter : IFormatter
+    {
+        public string Format(string format)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IsRegistered<TContainer>(string format) where TContainer : IFormatContainer
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TContainer>(string format) where TContainer : IFormatContainer
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unregister<TContainer>(string format) where TContainer : IFormatContainer
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
