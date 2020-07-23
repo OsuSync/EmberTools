206d7da426a9d83706a0db51c59aa505a169b687
Deliay <admin@remiliascarlet.com>
2020-07-23 22:53:26 +0800

    Redesign IFormatter interface

--- src/EmberKernel/Services/Statistic/IFormatter.cs
+++ src/EmberKernel/Services/Statistic/IFormatter.cs
@@ -1,16 +1,24 @@
-﻿using EmberKernel.Services.Statistic.Format;
+﻿using Autofac;
+using EmberKernel.Services.Statistic.Format;
+using System.Collections.Generic;
 
 namespace EmberKernel.Services.Statistic
 {
     public interface IFormatter
     {
-        void Register<TContainer>(string format)
+        void Register<TContainer>(ILifetimeScope scope, string format)
             where TContainer : IFormatContainer;
 
         void Unregister<TContainer>(string format)
             where TContainer : IFormatContainer;
 
+        void UnregisterAll<TContainer>()
+            where TContainer : IFormatContainer;
+
         void IsRegistered<TContainer>(string format)
+            where TContainer : IFormatContainer;
+
+        IEnumerable<string> GetRegisteredFormat<TContainer>()
             where TContainer : IFormatContainer;
 
         string Format(string format);
