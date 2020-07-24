85c64c1ef9394d390a54d3d57265618005b51eb7
Deliay <admin@remiliascarlet.com>
2020-07-24 10:06:53 +0800

    Complete EventSourceManager

---
+++ src/EmberKernel/Services/Statistic/DataSource/SourceManager/EventSourceManager.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using EmberKernel.Services.EventBus;
+using Microsoft.Extensions.Logging;
+
+namespace EmberKernel.Services.Statistic.DataSource.SourceManager
+{
+    public class EventSourceManager
+    {
+        private IEventBus EventBus { get; }
+        private ILogger<EventSourceManager> Logger { get; }
+        public EventSourceManager(IEventBus eventBus, ILogger<EventSourceManager> logger)
+        {
+            EventBus = eventBus;
+            Logger = logger;
+        }
+
+        public void Track<TEvent>(ILifetimeScope scope) where TEvent : Event<TEvent>
+        {
+            Logger.LogInformation($"Start tracking event {typeof(TEvent).Name}");
+            EventBus.Subscribe<TEvent, EventWrapperHandler<TEvent>>(scope);
+        }
+
+        public void Untrack<TEvent>() where TEvent : Event<TEvent>
+        {
+            EventBus.Unsubscribe<TEvent, EventWrapperHandler<TEvent>>();
+        }
+    }
+}
---
+++ src/EmberKernel/Services/Statistic/StatisticBuilder.cs
@@ -0,0 +1,55 @@
+﻿using Autofac;
+using EmberKernel.Services.Statistic.DataSource;
+using EmberKernel.Services.Statistic.DataSource.SourceManager;
+using EmberKernel.Services.Statistic.Formatter;
+
+namespace EmberKernel.Services.Statistic
+{
+    public class StatisticBuilder
+    {
+        public KernelBuilder Builder { get; set; }
+        public StatisticBuilder(KernelBuilder builder)
+        {
+            Builder = builder;
+        }
+
+        public StatisticBuilder ConfigureEventSourceManager()
+        {
+            Builder.Configure((context, container) =>
+            {
+                container.RegisterType<EventSourceManager>().SingleInstance();
+            });
+            return this;
+        }
+
+        public StatisticBuilder ConfigureFormatter<TFormatter>()
+            where TFormatter : IFormatter
+        {
+            Builder.Configure((context, container) =>
+            {
+                container.RegisterType<TFormatter>().As<IFormatter>().SingleInstance();
+            });
+            return this;
+        }
+
+        public StatisticBuilder ConfigureDataSource<TDataSource>()
+            where TDataSource : IDataSource
+        {
+            Builder.Configure((context, container) =>
+            {
+                container.RegisterType<TDataSource>().As<IDataSource>().SingleInstance();
+            });
+            return this;
+        }
+
+        public StatisticBuilder ConfigureDefaultDataSource()
+        {
+            return ConfigureDataSource<EmberDataSource>();
+        }
+
+        public StatisticBuilder ConfigureDefaultFormatter()
+        {
+            return ConfigureFormatter<EmberFormatter>();
+        }
+    }
+}
