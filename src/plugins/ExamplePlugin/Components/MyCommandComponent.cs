86717a02d73c66ee171998efc9a853dae74d0905
Deliay <admin@remiliascarlet.com>
2020-07-29 02:21:19 +0800

    statistic pattern example

--- src/plugins/ExamplePlugin/Components/MyCommandComponent.cs
+++ src/plugins/ExamplePlugin/Components/MyCommandComponent.cs
@@ -1,35 +1,43 @@
-﻿using EmberKernel.Services.Command.Attributes;
-using EmberKernel.Services.Command.Components;
+﻿using Autofac;
 using EmberKernel.Plugins;
 using EmberKernel.Plugins.Components;
-using EmberKernel.Services.EventBus;
+using EmberKernel.Services.Command.Attributes;
+using EmberKernel.Services.Command.Components;
+using EmberKernel.Services.Command.Models;
+using EmberKernel.Services.Configuration;
+using EmberKernel.Services.Statistic;
+using EmberKernel.Services.Statistic.Format;
 using ExamplePlugin.Commands;
 using ExamplePlugin.Models;
-using ExamplePlugin.Models.EventPublisher;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using EmberKernel.Services.Command;
-using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Configuration;
-using EmberKernel.Services.Configuration;
-using EmberKernel.Services.Command.Models;
+using System.Threading.Tasks;
 
 namespace ExamplePlugin.Components
 {
     [CommandContainerNamespace("my")]
-    public class MyCommandComponent : IComponent, ICommandContainer
+    public class MyCommandComponent : IComponent, ICommandContainer, IFormatContainer
     {
         private ILogger<MyCommandComponent> Logger { get; }
         private IPluginsManager PluginsManager { get; }
         private IPluginOptions<MyPlugin, MyPluginConfiguration> Config { get; }
+        private IFormatter Formatter { get; }
+        private IDataSource DataSource { get; }
+        private ILifetimeScope Scope { get; }
 
-        public MyCommandComponent(ILogger<MyCommandComponent> logger, IPluginsManager pluginsManager, IPluginOptions<MyPlugin, MyPluginConfiguration> config)
+        public MyCommandComponent(
+            ILifetimeScope scope,
+            ILogger<MyCommandComponent> logger,
+            IPluginsManager pluginsManager,
+            IPluginOptions<MyPlugin, MyPluginConfiguration> config,
+            IFormatter formatter,
+            IDataSource dataSource)
         {
+            Scope = scope;
             Logger = logger;
             PluginsManager = pluginsManager;
             Config = config;
+            Formatter = formatter;
+            DataSource = dataSource;
         }
 
         [CommandHandler(Command = "my")]
@@ -52,6 +60,24 @@ namespace ExamplePlugin.Components
             Logger.LogInformation($"Latest beatmap file= {Config.Create().LatestBeatmapFile}");
         }
 
+        [CommandHandler(Command = "var")]
+        [CommandAlias("var")]
+        public void Variables()
+        {
+            foreach (var item in DataSource.Variables)
+            {
+                Logger.LogInformation($"[{item.Id}]Name={item.Name},Value={item.Value.GetString()}");
+            }
+        }
+
+        [CommandHandler(Command = "format")]
+        [CommandAlias("format")]
+        public void Format(string format)
+        {
+            Formatter.Register<MyCommandComponent>(Scope, format);
+            Logger.LogInformation($"Format Registered:{format}");
+        }
+
         public void Dispose()
         {
         }
@@ -60,6 +86,12 @@ namespace ExamplePlugin.Components
         {
             newArgument = argument.MoveToHelpCommand();
             return true;
+        }
+
+        public ValueTask FormatUpdated(string format, string value)
+        {
+            Logger.LogInformation($"Format:{format},Result:{value}");
+            return default;
         }
     }
 }
---
+++ src/plugins/osu/EmberMemoryReader/MemoryReaderStatistic.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using EmberKernel.Plugins;
+using EmberKernel.Plugins.Attributes;
+using EmberKernel.Plugins.Components;
+using EmberKernel.Services.Statistic.Extension;
+using EmberMemoryReader.Abstract.Data;
+using System.Threading.Tasks;
+
+namespace EmberMemoryReader
+{
+    [EmberPlugin(Author = "Deliay", Name = "Ember Memory Reader Statistic", Version = "0.0.1")]
+    public class MemoryReaderStatistic : Plugin
+    {
+        public override void BuildComponents(IComponentBuilder builder)
+        {
+            builder.ConfigureEventStatistic<BeatmapInfo>();
+        }
+
+        public override ValueTask Initialize(ILifetimeScope scope)
+        {
+            scope.Track<BeatmapInfo>();
+            return default;
+        }
+
+        public override ValueTask Uninitialize(ILifetimeScope scope)
+        {
+            scope.Track<BeatmapInfo>();
+            return default;
+        }
+    }
+}
