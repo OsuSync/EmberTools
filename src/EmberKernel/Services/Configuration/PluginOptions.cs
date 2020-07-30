1deb3d0aa21ea3cd000410c46bcab7d4e1861a7f
Deliay <admin@remiliascarlet.com>
2020-07-30 20:29:36 +0800

    Split PluginOptions written feature to OptionWriter

--- src/EmberKernel/Services/Configuration/PluginOptions.cs
+++ src/EmberKernel/Services/Configuration/PluginOptions.cs
@@ -1,57 +1,24 @@
-﻿using Autofac;
-using EmberKernel.Plugins;
+﻿using EmberKernel.Plugins;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace EmberKernel.Services.Configuration
 {
-    public class PluginOptions<TPlugin, TOptions> : IPluginOptions<TPlugin, TOptions>
+    public class PluginOptions<TPlugin, TOptions> : OptionWriter<TOptions>, IPluginOptions<TPlugin, TOptions>
         where TPlugin : Plugin
         where TOptions : class, new()
     {
         private IOptionsFactory<TOptions> Factory { get; }
-        private readonly PluginOptionsSetting Setting;
-        public PluginOptions(ILifetimeScope scope, IOptionsFactory<TOptions> options)
+        public PluginOptions(IOptionsFactory<TOptions> options, PluginOptionsSetting setting)
+            : base(setting, Namespace)
         {
-            if (!scope.TryResolve(out Setting))
-            {
-                throw new NullReferenceException("Kernel not configre with .UseOptionsModerator, ensure call UseOptionsModerator when you build your life scope");
-            }
             this.Factory = options;
         }
 
-        private string Namespace => $"{typeof(TPlugin).Name}.{typeof(TOptions).Name}";
+        private static string Namespace => $"{typeof(TPlugin).Name}.{typeof(TOptions).Name}";
 
         public TOptions Create()
         {
             return Factory.Create($"{typeof(TPlugin).Name}.{typeof(TOptions).Name}");
-        }
-
-        public async ValueTask SaveAsync(TOptions options, CancellationToken cancellationToken = default)
-        {
-            Dictionary<string, object> dict = default;
-            using (var readStream = File.OpenRead(Setting.PersistFilePath))
-            {
-                dict = await JsonSerializer.DeserializeAsync<Dictionary<string, object>>(readStream, cancellationToken: cancellationToken);
-                dict.Remove(Namespace);
-                dict.Add(Namespace, options);
-            }
-            if (dict == null)
-            {
-                throw new NullReferenceException("Read persisted configuration errored!");
-            }
-            var tempFile = Path.GetTempFileName();
-            using (var writeStream = File.OpenWrite(tempFile))
-            {
-                await JsonSerializer.SerializeAsync(writeStream, dict, cancellationToken: cancellationToken);
-            }
-            File.Copy(tempFile, Setting.PersistFilePath, overwrite: true);
         }
     }
 }
