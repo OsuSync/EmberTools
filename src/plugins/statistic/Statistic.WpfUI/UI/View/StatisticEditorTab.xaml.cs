19321b2e5d0590b02e7ad32022e09c13471facb5
Deliay <admin@remiliascarlet.com>
2020-07-31 00:46:51 +0800

    statistic front UI project

---
+++ src/plugins/statistic/Statistic.WpfUI/UI/View/StatisticEditorTab.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using EmberKernel.Services.UI.Mvvm.ViewComponent;
+using Statistic.WpfUI.UI.ViewModel;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Statistic.WpfUI.UI.View
+{
+    /// <summary>
+    /// Interaction logic for StatisticEditorTab.xaml
+    /// </summary>
+    [ViewComponentNamespace(@namespace: "CoreWpfTab")]
+    public partial class StatisticEditorTab : UserControl, IViewComponent
+    {
+        public StatisticEditorTab()
+        {
+            InitializeComponent();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public ValueTask Initialize(ILifetimeScope scope)
+        {
+            this.DataContext = scope.Resolve<StatisticEditorViewModel>();
+            return default;
+        }
+
+        public ValueTask Uninitialize(ILifetimeScope scope)
+        {
+            this.DataContext = null;
+            return default;
+        }
+    }
+}
---
+++ src/plugins/statistic/Statistic.WpfUI/UI/ViewModel/StatisticEditorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using EmberKernel.Services.Statistic;
+using System.ComponentModel;
+using IComponent = EmberKernel.Plugins.Components.IComponent;
+
+namespace Statistic.WpfUI.UI.ViewModel
+{
+    class StatisticEditorViewModel : IComponent, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IStatisticHub Formats { get; set; }
+        public IDataSource Variables { get; set; }
+
+        public void Dispose() { }
+    }
+}
