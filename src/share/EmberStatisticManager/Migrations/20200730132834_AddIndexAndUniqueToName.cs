6a62d3112a3e7de842fc1054afdc31206a4af770
Deliay <admin@remiliascarlet.com>
2020-07-31 00:41:14 +0800

    Add statistic database

---
+++ src/share/EmberStatisticManager/Migrations/20200730132834_AddIndexAndUniqueToName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmberStatisticDatabase.Migrations
+{
+    public partial class AddIndexAndUniqueToName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_RegisteredFormats_Name",
+                table: "RegisteredFormats",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RegisteredFormats_Name",
+                table: "RegisteredFormats");
+        }
+    }
+}
---
+++ src/share/EmberStatisticManager/StatisticDatabaseContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using EmberSqlite.Integration;
+using EmberStatisticDatabase.Database;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EmberStatisticDatabase
+{
+    public class StatisticContextFactory : IDesignTimeDbContextFactory<StatisticContext>
+    {
+        public StatisticContext CreateDbContext(string[] args)
+        {
+            return new StatisticContext(new SqliteConfiguration("migrate.db"));
+        }
+
+    }
+}
