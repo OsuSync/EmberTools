34b18c7e3e038334b48c3b7271d3081ce401005d
Deliay <admin@remiliascarlet.com>
2020-08-02 13:58:07 +0800

    Expose variables and format via HTTP

---
+++ src/share/SimpleHttpServer/Host/ServerListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer.Host
+{
+    public class ServerListener : IDisposable
+    {
+        private HttpListener Listener { get; set; }
+        public ServerListener(HttpListener httpListener)
+        {
+            Listener = httpListener;
+            Listener.Start();
+        }
+
+        public event Func<HttpListenerContext, CancellationToken, ValueTask> ProcessRequest;
+        public async ValueTask HandleRequest(CancellationToken cancellationToken = default)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                ProcessRequest?.Invoke(await Listener.GetContextAsync(), cancellationToken);
+            }
+        }
+
+        public void Dispose()
+        {
+            Listener.Stop();
+        }
+    }
+}
